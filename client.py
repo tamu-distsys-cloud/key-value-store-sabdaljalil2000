516a9d8d2ba92d1dd0a741eb097e1bfa06facef2
sabdaljalil <sabdaljalil@hbku.edu.qa>
2025-06-20 16:43:12 +0300

    commit

--- client.py
+++ client.py
@@ -8,12 +8,14 @@ from server import GetArgs, GetReply, PutAppendArgs, PutAppendReply
 def nrand() -> int:
     return random.getrandbits(62)
 
+
 class Clerk:
     def __init__(self, servers: List[ClientEnd], cfg):
         self.servers = servers
         self.cfg = cfg
-
-        # Your definitions here.
+        self.client_id = nrand()
+        self.request_id = 0
+        self.lock = threading.Lock()
 
     # Fetch the current value for a key.
     # Returns "" if the key does not exist.
@@ -27,7 +29,29 @@ class Clerk:
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
     def get(self, key: str) -> str:
-        # You will have to modify this function.
+        with self.lock:
+            self.request_id += 1
+            req_id = self.request_id
+
+        args = GetArgs(key, self.client_id, req_id)
+        nshards = self.cfg.nservers
+        nreplicas = self.cfg.nreplicas
+        # shard = int(key) % nshards
+        try:
+            shard = int(key) % nshards
+        except ValueError:
+            shard = ord(key[0]) % nshards
+        servers_l = [(shard + i) % nshards for i in range(nreplicas)]
+        start = 0
+        while True:
+            i = servers_l[start]
+            start = (start + 1) % len(servers_l)
+            try:
+                reply = self.servers[i].call("KVServer.Get", args)
+                if reply is not None and isinstance(reply, GetReply):
+                    return reply.value
+            except TimeoutError:
+                continue
         return ""
 
     # Shared by Put and Append.
@@ -40,7 +64,29 @@ class Clerk:
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
     def put_append(self, key: str, value: str, op: str) -> str:
-        # You will have to modify this function.
+        with self.lock:
+            self.request_id += 1
+            req_id = self.request_id
+
+        args = PutAppendArgs(key, value, self.client_id, req_id)
+        nshards = self.cfg.nservers
+        nreplicas = self.cfg.nreplicas
+        # shard = int(key) % nshards
+        try:
+            shard = int(key) % nshards
+        except ValueError:
+            shard = ord(key[0]) % nshards
+        servers_l = [(shard + i) % nshards for i in range(nreplicas)]
+        start = 0
+        while True:
+            i = servers_l[start]
+            start = (start + 1) % len(servers_l)
+            try:
+                reply = self.servers[i].call("KVServer."+op, args)
+                if reply is not None and isinstance(reply, PutAppendReply):
+                    return reply.value
+            except TimeoutError:
+                continue
         return ""
 
     def put(self, key: str, value: str):
@@ -49,3 +95,8 @@ class Clerk:
     # Append value to key's value and return that value
     def append(self, key: str, value: str) -> str:
         return self.put_append(key, value, "Append")
+
+    
+######refs#####
+
+#for the fifth test case, i kept getting the error ValueError: invalid literal for int() with base 10: 'k'. to fix it, I used ChatGPT, which suggested to change shard = int(key) % nshards to try except catch error 
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import logging
 import threading
-from typing import Tuple, Any
+from typing import Dict, Tuple, Any
 
 debugging = False
 
@@ -11,10 +11,11 @@ def debug(format, *args):
 
 # Put or Append
 class PutAppendArgs:
-    # Add definitions here if needed
-    def __init__(self, key, value):
+    def __init__(self, key, value, client_id, request_id):
         self.key = key
         self.value = value
+        self.client_id = client_id
+        self.request_id = request_id
 
 class PutAppendReply:
     # Add definitions here if needed
@@ -22,9 +23,10 @@ class PutAppendReply:
         self.value = value
 
 class GetArgs:
-    # Add definitions here if needed
-    def __init__(self, key):
+    def __init__(self, key, client_id, request_id):
         self.key = key
+        self.client_id = client_id
+        self.request_id = request_id
 
 class GetReply:
     # Add definitions here if needed
@@ -35,26 +37,118 @@ class KVServer:
     def __init__(self, cfg):
         self.mu = threading.Lock()
         self.cfg = cfg
-
-        # Your definitions here.
+        self.kv_dict = {}
+        self.client_last = {}
+        self.nshards = cfg.nservers
+        self.nreplicas = cfg.nreplicas
 
     def Get(self, args: GetArgs):
-        reply = GetReply(None)
+        server_id = self.cfg.kvservers.index(self)
+        try:
+            shard = int(args.key) % self.nshards
+        except ValueError:
+            shard = ord(args.key[0]) % self.nshards
+        group = []
+        for i in range(self.nreplicas):
+            group.append((shard + i) % self.nshards)
 
-        # Your code here.
+        if server_id not in group:
+            return None
+        primary_id = None
+        for rid in group:
+            if rid in self.cfg.running_servers:
+                primary_id = rid
+                break
+        # if primary_id != server_id:
+        #     return None
+        with self.mu:
+            if args.client_id in self.client_last:
+                last_id, last_reply = self.client_last[args.client_id]
+                if args.request_id <= last_id:
+                    return last_reply
+            val = self.kv_dict.get(args.key, "")
+            reply = GetReply(val)
+            self.client_last[args.client_id] = (args.request_id, reply)
+            return reply
 
-        return reply
 
     def Put(self, args: PutAppendArgs):
-        reply = PutAppendReply(None)
-
-        # Your code here.
-
+        server_id = self.cfg.kvservers.index(self)
+        try:
+            shard = int(args.key) % self.nshards
+        except ValueError:
+            shard = ord(args.key[0]) % self.nshards
+        group = []
+        for i in range(self.nreplicas):
+            group.append((shard + i) % self.nshards)
+        if server_id not in group:
+            return None
+        primary_id = None
+        for rid in group:
+            if rid in self.cfg.running_servers:
+                primary_id = rid
+                break
+        # if primary_id != server_id:
+        #     return None
+        with self.mu:
+            if args.client_id in self.client_last:
+                last_id, last_reply = self.client_last[args.client_id]
+                if args.request_id <= last_id:
+                    return last_reply
+            self.kv_dict[args.key] = args.value
+            reply = PutAppendReply("")
+            self.client_last[args.client_id] = (args.request_id, reply)
+        for rid in group:
+            if rid == server_id or rid not in self.cfg.running_servers:
+                continue
+            replica = self.cfg.kvservers[rid]
+            with replica.mu:
+                last = replica.client_last.get(args.client_id, (-1, None))[0]
+                if args.request_id > last:
+                    replica.kv_dict[args.key] = args.value
+                    replica.client_last[args.client_id] = (args.request_id, PutAppendReply(""))
         return reply
 
     def Append(self, args: PutAppendArgs):
-        reply = PutAppendReply(None)
-
-        # Your code here.
-
+        server_id = self.cfg.kvservers.index(self)
+        try:
+            shard = int(args.key) % self.nshards
+        except ValueError:
+            shard = ord(args.key[0]) % self.nshards
+        group = []
+        for i in range(self.nreplicas):
+            group.append((shard + i) % self.nshards)
+            
+        if server_id not in group:
+            return None
+        primary_id = None
+        for rid in group:
+            if rid in self.cfg.running_servers:
+                primary_id = rid
+                break
+        # if primary_id != server_id:
+        #     return None
+        with self.mu:
+            if args.client_id in self.client_last:
+                last_id, last_reply = self.client_last[args.client_id]
+                if args.request_id <= last_id:
+                    return last_reply
+            old = self.kv_dict.get(args.key, "")
+            new = old + args.value
+            self.kv_dict[args.key] = new
+            reply = PutAppendReply(old)
+            self.client_last[args.client_id] = (args.request_id, reply)
+        for rid in group:
+            if rid == server_id or rid not in self.cfg.running_servers:
+                continue
+            replica = self.cfg.kvservers[rid]
+            with replica.mu:
+                last = replica.client_last.get(args.client_id, (-1, None))[0]
+                if args.request_id > last:
+                    rep_old = replica.kv_dict.get(args.key, "")
+                    replica.kv_dict[args.key] = rep_old + args.value
+                    replica.client_last[args.client_id] = (args.request_id, PutAppendReply(rep_old))
         return reply
+
+######refs#####
+#for the fifth test, i kept getting the error ValueError: invalid literal for int() with base 10: 'k'. to fix it, I used CHatGPT, which suggested to change 'shard = int(key) % nshards to try except catch error 
